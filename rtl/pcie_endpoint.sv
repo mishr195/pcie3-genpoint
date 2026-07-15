60d17cecad159ca30d8d8e9d24795e74dec87296
Yash <mishr195@purdue.edu>
2026-07-15 13:29:26 +0530

    Gate PCIe tx_ready on tx_valid

--- rtl/pcie_endpoint.sv
+++ rtl/pcie_endpoint.sv
@@ -224,6 +224,6 @@ module pcie_endpoint #(
         end
     end
 
-    assign tx_ready = link_up && ((rx_st != RX_IDLE) || (tx_st == TX_IDLE));
+    assign tx_ready = link_up && (tx_valid || (rx_st != RX_IDLE) || (tx_st == TX_IDLE));
 
 endmodule : pcie_endpoint
